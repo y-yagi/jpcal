b52168f60c87f3d91b5391f5cd18ba6e4543bbc7
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-04-03 08:14:51 +0900

    add test for without args

--- main_test.go
+++ main_test.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestRunVersion(t *testing.T) {
@@ -16,6 +18,22 @@ func TestRunVersion(t *testing.T) {
 	}
 
 	expected := "version: 1.0.0"
+	if !strings.Contains(out.String(), expected) {
+		t.Errorf("Expect out is %q, but %q", out.String(), expected)
+	}
+}
+
+func TestRunWithoutArgs(t *testing.T) {
+	out, err := new(bytes.Buffer), new(bytes.Buffer)
+	args := strings.Split("jpcal", " ")
+
+	status := run(args, out, err)
+	if status != 0 {
+		t.Errorf("Expect status is 0, but %d", status)
+	}
+
+	date := time.Now()
+	expected := fmt.Sprintf("     %d年 %02d月       ", date.Year(), date.Month())
 	if !strings.Contains(out.String(), expected) {
 		t.Errorf("Expect out is %q, but %q", out.String(), expected)
 	}
