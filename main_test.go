1fb74852e58bba94d37c86620cc5770724f5905a
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-04-03 08:27:02 +0900

    add more test

--- main_test.go
+++ main_test.go
@@ -38,3 +38,46 @@ func TestRunWithoutArgs(t *testing.T) {
 		t.Errorf("Expect out is %q, but %q", out.String(), expected)
 	}
 }
+
+func TestRunWithSpecifyYear(t *testing.T) {
+	out, err := new(bytes.Buffer), new(bytes.Buffer)
+	args := strings.Split("jpcal -y 2017", " ")
+
+	status := run(args, out, err)
+	if status != 0 {
+		t.Errorf("Expect status is 0, but %d", status)
+	}
+
+	expected := "2017年 01月            2017年 02月            2017年 03月"
+	if !strings.Contains(out.String(), expected) {
+		t.Errorf("Expect out is %q, but %q", out.String(), expected)
+	}
+}
+
+func TestRunWithThree(t *testing.T) {
+	out, err := new(bytes.Buffer), new(bytes.Buffer)
+	args := strings.Split("jpcal -3", " ")
+
+	status := run(args, out, err)
+	if status != 0 {
+		t.Errorf("Expect status is 0, but %d", status)
+	}
+
+	date := time.Now()
+	expected := fmt.Sprintf("%d年 %02d月", date.Year(), date.Month())
+	if !strings.Contains(out.String(), expected) {
+		t.Errorf("Expect out is %q, but %q", out.String(), expected)
+	}
+
+	lastMonth := beginningOfMonth(date).AddDate(0, 0, -1)
+	expected = fmt.Sprintf("%d年 %02d月", lastMonth.Year(), lastMonth.Month())
+	if !strings.Contains(out.String(), expected) {
+		t.Errorf("Expect out is %q, but %q", out.String(), expected)
+	}
+
+	nextMonth := beginningOfMonth(date).AddDate(0, 0, 1)
+	expected = fmt.Sprintf("%d年 %02d月", nextMonth.Year(), nextMonth.Month())
+	if !strings.Contains(out.String(), expected) {
+		t.Errorf("Expect out is %q, but %q", out.String(), expected)
+	}
+}
